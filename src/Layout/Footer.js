89a4438c4f362fca773a51a96cee5a750b907865
sb9479 <sb9479@naver.com>
2022-10-28 15:40:55 +0900

    ㅠㅠ

---
+++ src/Layout/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+    bottom: 100px;
+    margin-bottom: auto;
+    background-color: #dde0ea;
+`;
+
+// const Contents =styled.contents`
+//     width: 96%;
+//     max-width: 1100px;
+//     height: 100%;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     margin: 0 auto;
+// `;
+
+// const Title = styled.title`
+//     font-size: 20px;
+//     font-weight: 600;
+// `;
+
+
+const FooterVi = () => {
+    return (
+        <Footer>dk아아
+            {/* <Contents >
+                <Title>
+                    ㅠㅠㅠㅠㅠㅠㅠ
+                </Title>
+            </Contents> */}
+        </Footer>
+
+    );
+}
+
+export default FooterVi;
---
+++ src/pages/MyPage.js
@@ -0,0 +1,15 @@
+
+import {useUserState} from 'react';
+
+const MyPage = () => {
+    const { user } = useUserState();
+    return (
+        <>
+        <p>Mypage</p>
+        <div>
+            
+        </div>
+        </>
+    );
+}
+export default MyPage;
